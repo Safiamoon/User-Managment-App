d00ebe43d6d3fb770ba1b4cbafbfb94417dab9b3
Safiamoon <msellek.safia@gmail.com>
2021-01-23 19:32:07 +0100

    add navbar an pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import Contact from "./components/pages/Contact";
+import Navbar from './components/layouts/Navbar';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import NotFound from './components/pages/NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router> 
+    <div>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home}></Route>
+        <Route exact path="/about" component={About}></Route>
+        <Route exact path="/contact" component={Contact}></Route>
+        <Route component={NotFound}></Route>
+      </Switch>
     </div>
+    </Router>
   );
 }
 
